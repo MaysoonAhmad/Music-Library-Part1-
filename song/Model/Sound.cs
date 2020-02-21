6bc2052367655877543d7a4595d6926d9eabb5b1
MaysoonAhmad <31833085+MaysoonAhmad@users.noreply.github.com>
2020-02-21 11:37:58 -0800

    commit stahe

---
+++ song/Model/Sound.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace song.Model
+{
+    public enum SoundCategory
+
+    {
+
+        Animals,
+
+        Cartoons,
+
+        Taunts,
+
+        Warnings,
+        Sia,
+        Justin,
+        Adele,
+        Taylor
+    }
+
+    public class Sound
+
+    {
+
+        public string Name { get; set; }
+
+        public SoundCategory Category { get; set; }
+
+        public string AudioFile { get; set; }
+
+
+
+        public string ImageFile { get; set; }
+
+
+
+
+
+        public Sound(string name, SoundCategory category)
+
+        {
+
+            Name = name;
+
+            Category = category;
+
+            AudioFile = $"/Assets/Audio/{category}/{name}.wav";
+
+            ImageFile = $"/Assets/Images/{category}/{name}.png";
+
+        }
+
+    }
+
+}
---
+++ song/Model/SoundManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+/// <summary>
+/// /////////////////////////////
+/// </summary>
+namespace song.Model
+{
+    public static class SoundManager
+    {
+        private static List<Sound> getSounds()
+
+        {
+
+            var sounds = new List<Sound>();
+
+
+
+            sounds.Add(new Sound("Hello", SoundCategory.Adele));
+
+            sounds.Add(new Sound("Someone", SoundCategory.Adele));
+
+
+
+            sounds.Add(new Sound("What", SoundCategory.Justin));
+
+            sounds.Add(new Sound("Yummy", SoundCategory.Justin));
+
+
+
+            sounds.Add(new Sound("Chandelier", SoundCategory.Sia));
+
+            sounds.Add(new Sound("Cheap", SoundCategory.Sia));
+
+
+
+            sounds.Add(new Sound("Look", SoundCategory.Taylor));
+
+            sounds.Add(new Sound("Shake", SoundCategory.Taylor));
+
+
+
+            return sounds;
+
+        }
+
+
+
+        public static void GetAllSounds(ObservableCollection<Sound> sounds)
+
+        {
+
+            var allSounds = getSounds();
+
+            sounds.Clear();
+
+            allSounds.ForEach(s => sounds.Add(s));
+
+        }
+
+
+
+        public static void GetSoundsByCategory(
+
+            ObservableCollection<Sound> sounds, SoundCategory category)
+
+        {
+
+            var allSounds = getSounds();
+
+            var filteredSounds = allSounds.Where(s => s.Category == category).ToList();
+
+            sounds.Clear();
+
+            filteredSounds.ForEach(s => sounds.Add(s));
+
+        }
+
+
+
+    }
+
+}
